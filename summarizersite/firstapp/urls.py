e35f86cf6acd867266c9bd5f8fbf086b26c399a5
LAPTOP-4M7RJIRA\rutup <rutupatel41299@gmail.com>
2024-06-13 09:48:50 -0700

    adding API paths to urls.py

--- summarizersite/firstapp/urls.py
+++ summarizersite/firstapp/urls.py
@@ -5,7 +5,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    
+    path('', views.login_request, name='login_request'),
+    path('register/', views.register, name='register'),
+    path('index/', views.index, name='index'),
+    path('logout_request', views.logout_request, name='logout_request'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('summarizer/', csrf_exempt(views.summarizer), name='summarizer'),
+    path('summarizer1/', csrf_exempt(views.summarizer1), name='summarizer1'),
+    path('feedback/', views.feedback, name='feedback'),
+    path('forgotpassword/', views.forgotpassword, name='forgotpassword'),
+    path('subscribe/', views.subscribe, name='subscribe'),
 ]
 
 if settings.DEBUG:
