d074001cdb676d43fca6abe45e2c1a3eb3bbac97
LAPTOP-4M7RJIRA\rutup <rutupatel41299@gmail.com>
2024-06-16 13:04:09 -0700

    defining schema for use registration in models.py

--- summarizersite/firstapp/models.py
+++ summarizersite/firstapp/models.py
@@ -1,2 +1,17 @@
 from django.db import models
 
+class Registration(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    occupation = models.CharField(max_length=50)
+    email_id = models.EmailField(unique=True)
+    contact_no = models.IntegerField()
+    birth_date = models.DateField()
+    country = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = "Registration"
+        verbose_name_plural = "Registration"
+
+    def __str__(self):
+        return self.email_id
