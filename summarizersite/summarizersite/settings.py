cc1bbcd489422db5d71ce4c441adb624aad4b608
LAPTOP-4M7RJIRA\rutup <rutupatel41299@gmail.com>
2024-06-18 21:21:32 -0700

    adding db configurations to settings.py

--- summarizersite/summarizersite/settings.py
+++ summarizersite/summarizersite/settings.py
@@ -76,7 +76,21 @@ WSGI_APPLICATION = 'summarizersite.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    # 'default': {
+    #             'ENGINE': 'django.db.backends.mysql',
+    #             'NAME': 'db_summarizer',
+    #             'USER': 'root',
+    #             'PASSWORD': "",
+    #             'HOST': "localhost",
+    #             'PORT': "3306",
+    #             'OPTIONS': {
+    #             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+    #             }
+    #             }
 }
 
 
