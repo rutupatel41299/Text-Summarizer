531f83453c25ec515687786d2bf6ce9eddcaf003
LAPTOP-4M7RJIRA\rutup <rutupatel41299@gmail.com>
2024-06-24 11:34:04 -0700

    defining methods in views.py

--- summarizersite/firstapp/views.py
+++ summarizersite/firstapp/views.py
@@ -14,3 +14,26 @@ def index(request):
             return render(request, 'index.html')
     except:
         return redirect('login_request')
+
+def register(request):
+    if request.method == 'POST':
+        email_id = request.POST['email_id']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        birth_date = request.POST['birth_date']
+        contact_no = request.POST['contact_no']
+        occupation = request.POST['occupation']
+        country = request.POST['country']
+        regobj = Registration(first_name=first_name, last_name=last_name, occupation=occupation, email_id=email_id, contact_no=contact_no, birth_date=birth_date, country=country)
+        loginobj = Login(email_id=email_id, password=password, active_inactive=True)
+
+        try:
+            regobj.save()
+            loginobj.save()
+            return redirect('login_request')
+        except:
+            return render(request, 'auth-multi-step-sign-up.html')
+    else:
+        return render(request, 'auth-multi-step-sign-up.html')
