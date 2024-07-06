edbbdac7fdbff5d3f70817339d16ddb1c85e7ff2
LAPTOP-4M7RJIRA\rutup <rutupatel41299@gmail.com>
2024-07-05 17:27:56 -0700

    updated summary generation algo

--- summarizersite/firstapp/module.py
+++ summarizersite/firstapp/module.py
@@ -10,3 +10,18 @@ def handle_uploaded_file(f):
         for chunk in f.chunks():
             destination.write(chunk)
     return 'C:/Users/rutup/PycharmProjects/final_summarizer_project/summarizersite/files/name.txt'
+
+# processed data for fetching unique words
+def natural_language_processing(data):
+    # removing punctuations and new lines and tabs
+    pun = string.punctuation + '\t' + '—' + '‘’' + '\n' + '\r' + '\r\n' + '\v' + '\x0b' + '\f' + '\x0c' + '\x1c' + '\x1d' + '\x1e' + '\x85' + '\u2028' + '\u2029'
+    for sym in pun:
+        cnt = data.count(sym)
+        data = data.replace(sym, '', cnt)
+
+    # lower the data
+    data = data.lower()
+
+    doc = nlp(data)
+    print('processed data: ', doc)
+    return doc
